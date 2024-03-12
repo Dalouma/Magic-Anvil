a300fea7948dc524ed7cf038b330cf29ee7ad451
omegatm <jverespey@gmail.com>
2024-03-12 14:28:12 -0700

    Added loan button (Temp idea)

---
+++ Assets/loanAnim.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loanAnim : MonoBehaviour
+{
+    public ShopManager shopManager;
+    public CustomerManager customerManager;
+    public Animator animator;
+    public void LoanMoney()
+    {
+        shopManager.UPdateMoney(400);
+        customerManager.SetRep(CustomerManager.currRep - 10);
+    }
+
+    private void Update()
+    {
+        if (ShopManager.money < 200 && (ShopManager.materials[0]<1||ShopManager.materials[1] < 1))
+        {
+            animator.SetBool("showloan", true);
+        }
+        else
+        {
+            animator.SetBool("showloan", false);
+        }
+    }
+}
