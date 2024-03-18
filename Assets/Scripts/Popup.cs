8d7fbd53f1da89ff4d3bb833f776ecf57c43def7
UndeadKyuubi <91641904+UndeadKyuubi@users.noreply.github.com>
2024-03-17 22:05:58 -0700

    Added localization for most UI elements

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject popupObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("PopupShown")) 
+        {
+            popupObj.SetActive(true);
+
+            PlayerPrefs.SetInt("PopupShown", 1);
+        }
+        else 
+        {
+            popupObj.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
