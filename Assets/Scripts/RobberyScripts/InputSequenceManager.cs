9f7131af9d01589fa9dcb0115961e6e31646e520
UndeadKyuubi <91641904+UndeadKyuubi@users.noreply.github.com>
2024-04-24 14:32:16 -0700

    Added initial version of robbery minigame

---
+++ Assets/Scripts/RobberyScripts/InputSequenceManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputSequenceManager : MonoBehaviour
+{
+    public List<InputType> actionSequence;
+    private int currentAction = 0;
+    private Vector2 startPosition;
+
+    public TMP_Text sequenceText;
+
+    private List<InputType> testSequence = new List<InputType> {
+        InputType.Tap,
+        InputType.SwipeUp,
+        InputType.SwipeDown,
+        InputType.SwipeLeft,
+        InputType.SwipeRight
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actionSequence = testSequence;
+        UpdateSequenceText();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) 
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase) 
+            {
+                case TouchPhase.Began:
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    Vector2 endPosition = touch.position;
+                    CheckInput(GetSwipeDirection(startPosition, endPosition));
+                    break;
+            }
+        }
+    }
+
+    void CheckInput(InputType input) 
+    {
+        if (input == actionSequence[currentAction]) 
+        {
+            currentAction++;
+            if (currentAction >= actionSequence.Count) 
+            {
+                Debug.Log("Sequence Completed!");
+                currentAction = 0;
+                UpdateSequenceText();
+            }
+        }
+        else 
+        {
+            Debug.Log("Sequence Failed!");
+            currentAction = 0;
+            UpdateSequenceText();
+        }
+    }
+
+    InputType GetSwipeDirection(Vector2 start, Vector2 end) 
+    {
+        float swipeMagnitude = (end - start).magnitude;
+        if (swipeMagnitude < 20f) 
+        {
+            return InputType.Tap;
+        }
+
+        Vector2 swipeDirection = end - start;
+        float angle = Vector2.SignedAngle(swipeDirection, Vector2.right);
+
+        if (angle > -45f && angle <= 45f) 
+        {
+            return InputType.SwipeRight;
+        }
+        else if (angle > 45f && angle <= 135f) 
+        {
+            return InputType.SwipeDown;
+        }
+        else if (angle > -135f && angle <= -45f) 
+        {
+            return InputType.SwipeUp;
+        }
+        else 
+        {
+            return InputType.SwipeLeft;
+        }
+    }
+
+    void UpdateSequenceText() 
+    {
+        string sequenceString = "Sequence: ";
+        foreach (var action in actionSequence) 
+        {
+            sequenceString += action.ToString() + " ";
+        }
+        sequenceText.text = sequenceString;
+    }
+}
+
+public enum InputType 
+{
+    Tap,
+    SwipeUp,
+    SwipeDown,
+    SwipeLeft,
+    SwipeRight
+}
