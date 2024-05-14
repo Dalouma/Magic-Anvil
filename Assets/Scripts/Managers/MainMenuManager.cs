44f3e54083bcdd8bf062cdaed2b91f3c1d450b75
Trevor Gardner <trevorthebike@gmail.com>
2024-05-14 01:11:24 -0700

    added new popup to start screen, to warn users before creating new game

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    
+    public Canvas startMenu;
     
     public AudioSource musicSource;
     // Start is called before the first frame update
@@ -13,6 +14,17 @@ public class MainMenuManager : MonoBehaviour
     {
         AudioManager.instance.ChangeMusic("menu");
     }
+
+    public void ToggleNewGamePopup()
+    {
+        startMenu.gameObject.SetActive(true);
+    }
+
+     public void ExitNewGamePopup()
+    {
+        startMenu.gameObject.SetActive(false);
+    }
+
     /*
     // Update is called once per frame
     void Update()
