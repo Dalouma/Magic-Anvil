39873652ed24bbeda05a4a3c664c447a46c535aa
Jacob Dickerman <jwdicker@ucsc.edu>
2024-05-28 16:02:38 -0700

    Re-improved readability
    Also fixed bug where the music restarted after moving to the shop

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,8 +8,9 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     public AudioMixer mixer;
-    public List<AudioSource> bgmusicSources;
+    public List<AudioSource> musicSources;
     public List<AudioSource> sfxSources;
+    private string currPlaying;
 
     private void Awake()
     {
@@ -24,64 +24,109 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
-    private void StopAllMusic()
-    {
-        for (int i = 0; i < bgmusicSources.Count; i++)
-        {
-            bgmusicSources[i].Stop();
-        }
-    }
 
+    // Music Handling
     public enum MusicTrack
     {
         Menu,
         Forge,
-
-
+        Robbery
     }
-    public void ChangeMusic(string trackName)
+
+    private void StopAllMusic()
     {
+        for (int i = 0; i < musicSources.Count; i++)
+        {
+            musicSources[i].Stop();
+        }
+    }
+
+    public void ChangeMusic(string trackName, bool restart = false)
+    {
+        // If the music to change to is the same as the music currently playing, only change if specified
+        if(!restart && trackName == currPlaying) {
+            Debug.Log("Already Playing");
+            return;
+        }
+
+        // Track is different or restart was specified. Stop music and start a new track
         StopAllMusic();
-        if (trackName == "menu")
-        {
-            bgmusicSources[0].Play();
+        currPlaying = trackName;
+        Debug.Log($"Now Playing {trackName}");
+        switch (trackName) {
+            case "menu":
+                musicSources[0].Play();
+                break;
+            case "forge":
+                musicSources[1].Play();
+                break;
+            case "robbery":
+                musicSources[2].Play();
+                break;
+            default:
+                Debug.LogError($"Error: Invalid track name {trackName}");
+                break;
         }
-        else if (trackName == "forge")
-        {
-            bgmusicSources[1].Play();
-        }
-        else if (trackName == "robbery")
-        {
-            bgmusicSources[2].Play();
-        }
-    } 
-
-    public void PlaySFX(string sfxName)
-    {
-        if ( sfxName == "Anvil")
-        {
-            sfxSources[0].Play();
-        }
-        else if ( sfxName == "Victory")
-        {
-            sfxSources[1].Play();
-        }
-        else if ( sfxName == "SwordGrind")
-        {
-            sfxSources[2].Play();
-        }
-        else if ( sfxName == "Newspaper")
-        {
-            sfxSources[3].Play();
-        }
-        else if ( sfxName == "Coin")
-        {
-            sfxSources[4].Play();
-        }
-    
     }
 
+    // SFX handling
+    // Get the audio source corresponding to the provided string
+    public enum SFXTrack
+    {
+        Anvil,
+        Victory,
+        SwordGrind,
+        Newspaper,
+        Coin,
+    }
+
+    private AudioSource GetSound(string soundName) {
+        switch (soundName) {
+            case "Anvil":
+                return sfxSources[0];
+            case "Victory":
+                return sfxSources[1];
+            case "SwordGrind":
+                return sfxSources[2];
+            case "Newspaper":
+                return sfxSources[3];
+            case "Coin":
+                return sfxSources[4];
+            default:
+                Debug.LogError($"Error: Invalid SFX name {soundName}");
+                return null;
+        }
+    }
+
+    // Play the specified sound
+    public void PlaySFX(string soundName) {
+        // Determine which AudioSource to play
+        AudioSource src = GetSound(soundName);
+
+        // Play the sound
+        Debug.Log($"Now Playing sound {soundName}");
+        src.Play();
+    }
+
+    // Stop the specified sound
+    public void stopSound(string soundName) {
+        GetSound(soundName).Stop();
+    }
+
+    // Stop all sounds
+    public void stopAllSounds() {
+        for (int i = 0; i < sfxSources.Count; i++)
+        {
+            sfxSources[i].Stop();
+        }
+    }
+    
+    // Handle the button click sound
+    public static void click() {
+        instance.PlaySFX("click");
+    }
+
+    // Volume Handling
     public void SetBGVolume(){
         mixer.GetFloat("BGM", out float currVolume);
         if(currVolume == -40){
